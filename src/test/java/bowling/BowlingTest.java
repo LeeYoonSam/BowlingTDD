ea97ccca5277dacee358644df2345ac43f4792c0
YS <yoonsam86@gmail.com>
2018-09-07 22:21:00 +0900

    테스트 클래스 생성

---
+++ src/test/java/bowling/BowlingTest.java
@@ -0,0 +1,15 @@
+package bowling;
+
+/**
+ * Bowling
+ * 볼링 점수 계산 - http://stparkms.tistory.com/62
+ *
+ * roll(int n)과 score()를 구현
+ * 내부적으로 각 프레임(1-10)의 점수를 관리할 것
+ * spare, strike 점수 구현
+ * 범위를 초과했거나 횟수를 초과하여 roll을 호출하면 IllegalArgumentException과 IllegalStateException 발생
+ * 최저 점수와 최대 점수까지 테스트에 반드시 반영
+ * 새 테스트 통과마다 commit 해서 github에 올릴 것
+ */
+public class BowlingTest {
+}
